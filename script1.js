57bb84887a6d99a7506a766403509a64b5dbeb2d
ellyvandertogt <100711482+ellyvandertogt@users.noreply.github.com>
2022-12-30 16:46:07 +0100

    add second html

--- script1.js
+++ script1.js
@@ -7,19 +7,14 @@ canvas.height = window.innerHeight;
 window.addEventListener("resize", function() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight; 
-    // ctx.fillStyle = "white";
-    // ctx.fillRect(10, 20, 150, 50);
 });
 
-// ctx.fillStyle = "white";
-// ctx.fillRect(10, 20, 150, 50);
 
-//Drawing a circle
 ctx.fillStyle = "blue";
 ctx.strokeStyle = "red";
 ctx.lineWidth = 15;
 ctx.beginPath();
-ctx.arc(100, 100, 50, 0, Math.PI * 2); // startin point to draw 
+ctx.arc(100, 100, 50, 0, Math.PI * 2); // starting point to draw 
 ctx.stroke();
 ctx.fill();
 console.log(ctx);
